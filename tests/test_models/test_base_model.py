0a7eb51484030ffb39b16b5101b9bc94deb87854
Bhlak <bhlakcodes@proton.me>
2023-05-18 21:29:36 +0100

    Test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -9,4 +9,21 @@ from models.base_model import BaseModel
 
 
 class TestBaseModel(unittest.TestCase):
-    pass
+    """
+    The test suite for models.base_model.BaseModel
+    """
+
+    def test_if_BaseModel_instance_has_id(self):
+        """
+        Checks that instance has an id assigned on initialization
+        """
+        b = BaseModel()
+        self.assertTrue(hasattr(b, "id"))
+
+    def test_str_representation(self):
+        """
+        Checks if the string representation is appropriate
+        """
+        b = BaseModel()
+        self.assertEqual(str(b),
+                         "[BaseModel] ({}) {}".format(b.id, b.__dict__))
