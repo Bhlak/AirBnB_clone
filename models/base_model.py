86758cbdeb389130fd6551aabed638c780c2bbfd
Bhlak <bhlakcodes@proton.me>
2023-05-18 21:44:43 +0100

    Task 4

--- models/base_model.py
+++ models/base_model.py
@@ -11,13 +11,21 @@ class BaseModel:
     A class that defines the common attributes/methods for other classes
     """
 
-    def __init__(self):
+    def __init__(self, *args, **kwargs):
         """
         Initializes the BaseModel class
         """
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = self.created_at
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
+        else:
+            for k, v in kwargs.items():
+                if k != '__class__':
+                    if k in ['created_at', 'updated_at']:
+                        setattr(self, k, datetime.fromisoformat(v))
+                    else:
+                        setattr(self, k, v)
 
     def __str__(self):
         """
